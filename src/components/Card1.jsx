4e1f15bffd517e3ab4d26193925676f8448776bf
Abubakar <166229629+AbubakarShaikh123@users.noreply.github.com>
2024-07-26 21:30:17 +0530

    Update Card1.jsx

--- src/components/Card1.jsx
+++ src/components/Card1.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Card1({imgurl,title,des}) {
   return (
-    <div className='w-[250px] h-[320px] bg-transparent rounded-md border shadow-md shadow-white	cursor-pointer hover:scale-105 transition-all overflow-hidden text-ellipsis'>
+    <div className='w-[250px] h-[320px] bg-transparent rounded-sm border shadow-md shadow-white	cursor-pointer hover:scale-105 transition-all overflow-hidden text-ellipsis'>
         <img src={imgurl} alt="" className='w-full h-[70%] object-cover  rounded-md hover:scale-105 transition-all hover:overflow-hidden' />
         <h1 className='text-2xl font-bold pl-1 py-1'>${title}</h1>
         <p className='px-1 text-[16px] leading-5 text-[#bcbaba] text-ellipsis	overflow-hidden'>{des}</p>
